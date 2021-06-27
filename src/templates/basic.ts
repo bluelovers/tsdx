36a3bdfb2773ca6d7d5c9f0e64331990cb992a95
bluelovers <codelovers@users.sourceforge.net>
2021-06-28 05:31:30 +0800

    feat: add github coverage

--- src/templates/basic.ts
+++ src/templates/basic.ts
@@ -3,12 +3,12 @@ import { Template } from './template';
 const basicTemplate: Template = {
   name: 'basic',
   dependencies: [
-    'husky',
+    //'husky',
     //'tsdx',
     'tslib',
-    'typescript@next',
-    'size-limit',
-    '@size-limit/preset-small-lib',
+    //'typescript@next',
+    //'size-limit',
+    //'@size-limit/preset-small-lib',
     '@bluelovers/tsconfig'
   ],
   packageJson: {
@@ -19,7 +19,16 @@ const basicTemplate: Template = {
     main: 'dist/index.js',
     // module: `dist/${safeName}.esm.js`,
     typings: `dist/index.d.ts`,
-    files: ['dist', 'src'],
+    "files": [
+      "tsconfig.json",
+      "src/**/*.*",
+      "dist/**/*",
+      "!**/*.test.ts",
+      "!**/*.spec.ts",
+      "!**/*.test.js",
+      "!**/*.spec.js",
+      "!**/__snapshots__/*"
+    ],
     engines: {
       node: '>=10',
     },
@@ -34,6 +43,13 @@ const basicTemplate: Template = {
       prepare: 'tsdx build',
       size: 'size-limit',
       analyze: 'size-limit --why',
+      "npm:publish": "yarn run preversion && npm publish && yarn run postpublishOnly",
+      "npm:publish:bump": "yarn-tool version && yarn run preversion && npm publish && yarn run postpublishOnly",
+      "postpublish:changelog": "ynpx --quiet @yarn-tool/changelog && git add ./CHANGELOG.md",
+      "postpublish:git:commit": "git commit -m \"chore(release): publish\" . & echo postpublish:git:commit",
+      "postpublish:git:push": "git push --follow-tags",
+      "postpublish:git:tag": "ynpx --quiet @yarn-tool/tag",
+      "postpublishOnly": "yarn run postpublish:changelog && yarn run postpublish:git:commit && yarn run postpublish:git:tag && yarn run postpublish:git:push",
     },
     "devDependencies": {
       "@bluelovers/tsconfig": "^1.0.22",
